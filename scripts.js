792037fd8e6259b2cf618791cc5c3dad0668ea17
RecettesMemaine <recettes_memaine@outlook.com>
2025-11-12 20:31:11 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+<script>
+const openModalBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('myModal');
+const closeBtn = document.querySelector('.close');
+
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+</script>
